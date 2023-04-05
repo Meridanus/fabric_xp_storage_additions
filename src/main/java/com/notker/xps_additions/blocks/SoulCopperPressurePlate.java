cdc98f9ee162a33bde7eae6e3485d67cf439dd35
Meridanus <96318256+Meridanus@users.noreply.github.com>
2023-04-05 21:22:11 +0200

    update to 1.19.4

--- src/main/java/com/notker/xps_additions/blocks/SoulCopperPressurePlate.java
+++ src/main/java/com/notker/xps_additions/blocks/SoulCopperPressurePlate.java
@@ -1,10 +1,10 @@
 package com.notker.xps_additions.blocks;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.BlockSetType;
 import net.minecraft.block.Material;
 import net.minecraft.block.PressurePlateBlock;
 import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.sound.SoundEvents;
 
 public class SoulCopperPressurePlate extends PressurePlateBlock {
     public SoulCopperPressurePlate() {
@@ -14,7 +14,6 @@ public class SoulCopperPressurePlate extends PressurePlateBlock {
                 .strength(0.5f, 0.5f)
                 .nonOpaque()
                 .requiresTool(),
-                SoundEvents.BLOCK_IRON_DOOR_CLOSE,
-                SoundEvents.BLOCK_IRON_DOOR_OPEN);
+                BlockSetType.IRON);
     }
 }
